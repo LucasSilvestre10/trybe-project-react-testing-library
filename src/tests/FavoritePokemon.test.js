04490f2dbdd0b727c8efa7479ab038f88df3da47
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2022-11-22 19:55:54 -0300

    requisito 3 feito

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+describe('Teste do componente FavoritePokemon', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    // acessar
+    renderWithRouter(<FavoritePokemon />);
+
+    const element = screen.getByText(/no favorite pokémon found/i);
+
+    // interagir
+
+    // testar
+    expect(element).toHaveTextContent('No favorite Pokémon found');
+  });
+});
